0e42225601214dafda6fa82fd1340e0aba0c8a9c
jhs4132@gmail.com <jhs4132@gmail.com>
2023-04-20 17:45:24 +0900

    2023.4.20

--- src/main/java/com/kbstar/ServletInitializer.java
+++ src/main/java/com/kbstar/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Ws0420Application.class);
+        return application.sources(Clone0420Application.class);
     }
 
 }
